bb67c1812b5388311e493bf75e73f32a3ea52e3e
Daniel Wolf <danielrwolf5@gmail.com>
2017-02-22 11:15:12 -0600

    updating settings in phpunit.xml.dist, testing disque queue manager, added git attributes file to not download some files when composer installing

--- tests/Disque/DisqueQueueManagerTest.php
+++ tests/Disque/DisqueQueueManagerTest.php
@@ -2,26 +2,105 @@
 namespace Punchkick\QueueManager\Disque;
 
 use Disque\Client;
+use Disque\Queue\Job;
+use Disque\Queue\Queue;
 use PHPUnit\Framework\TestCase;
 use Punchkick\QueueManager\Disque\DisqueQueueManager;
 
 class DisqueQueueManagerTest extends TestCase
 {
     /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $mockClient;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $mockQueue;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $mockJob;
+
+    /**
      * @var DisqueQueueManager
      */
-    protected $instance;
+    protected $disqueQueueManager;
 
     public function setUp()
     {
-        $mockClient = $this->getMockBuilder(Client::class)
+        $this->mockClient = $this->getMockBuilder(Client::class)
+            ->setMethods(['queue'])
             ->disableOriginalConstructor()
             ->getMock();
-        $this->instance = new DisqueQueueManager($mockClient);
+
+        $this->mockQueue = $this->getMockBuilder(Queue::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['push', 'pull'])
+            ->getMock();
+
+        $this->mockJob = $this->getMockBuilder(Job::class)
+            ->getMock();
+
+        $this->disqueQueueManager = new DisqueQueueManager($this->mockClient);
     }
 
-    public function testIsTesting()
+    public function testAddJob()
     {
-        $this->markTestIncomplete();
+        $jobName = 'foo_bar';
+        $jobData = [
+            'foo' => 'bar',
+        ];
+
+        $this->mockClient->expects($this->once())
+            ->method('queue')
+            ->with($jobName)
+            ->willReturn($this->mockQueue);
+
+        $this->mockQueue->expects($this->once())
+            ->method('push')
+            ->with($this->callback(function (Job $job) use ($jobData) {
+                return $job->getBody() === $jobData;
+            }), ['ttl' => 604800]);
+
+        $this->assertTrue($this->disqueQueueManager->addJob($jobName, $jobData));
+    }
+
+    public function testGetJobEmptyQueue()
+    {
+        $jobName = 'foo_bar';
+
+        $this->mockClient->expects($this->once())
+            ->method('queue')
+            ->with($jobName)
+            ->willReturn($this->mockQueue);
+
+        $this->mockQueue->expects($this->once())
+            ->method('pull')
+            ->with(1000)
+            ->willReturn(null);
+
+        $this->expectException('\Punchkick\QueueManager\Exception\EmptyQueueException');
+        $this->disqueQueueManager->getJob($jobName);
+    }
+
+    public function testGetJobReturnsJob()
+    {
+        $jobName = 'foo_bar';
+
+        $this->mockClient->expects($this->once())
+            ->method('queue')
+            ->with($jobName)
+            ->willReturn($this->mockQueue);
+
+        $this->mockQueue->expects($this->once())
+            ->method('pull')
+            ->with(1000)
+            ->willReturn($this->mockJob);
+
+        $job = $this->disqueQueueManager->getJob($jobName);
+        $this->assertInstanceOf(DisqueJob::class, $job);
     }
 }
